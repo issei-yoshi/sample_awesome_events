82d6b0d817baa1daf5515cdaf8180fcf07befc3a
issei-yoshi <iyoshi.engineer@gmail.com>
2023-09-16 01:02:55 +0900

    defined actions on events_controller

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,10 @@
 class Event < ApplicationRecord
   validates :name, length: { maximum: 50 }, presence: true
   validates :place, length: { maximum: 100 }, presence: true
-  validates :content, , length: { maximum: 2000 }, presence: true
+  validates :content, length: { maximum: 2000 }, presence: true
   validates :start_at, presence: true
   validates :end_at, presence: true
-  valiate :start_at_should_be_before_end_at
+  validate :start_at_should_be_before_end_at
 
   private
 
