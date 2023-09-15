48225958e0efb9f49ece6b6bb772576475b28206
issei-yoshi <iyoshi.engineer@gmail.com>
2023-09-16 00:16:01 +0900

    create link for events/new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :events
   root 'hello#index'
   get 'auth/:provider/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  resources :events
 end
